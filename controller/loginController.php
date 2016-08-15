f6dbbdfd673867747240dab4366fe4468a6f8090
Olme Marin <olme_gms@hotmail.com>
2016-08-15 17:38:52 -0500

    commit
    olme_gms20160815: corrección del commit

--- controller/loginController.php
+++ controller/loginController.php
@@ -4,10 +4,7 @@ require_once ('../model/user/UserDao.class.php');
 session_start();
 
 $_SESSION['DIR_CONTROLLERS'] = dirname($_SERVER['PHP_SELF']);
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
 if(isset( $_SESSION["login"]) && $_SESSION["login"] == "valid"){
     header("Location: ../view/modules/instructor/instructor.php");
 }else{
@@ -15,10 +12,7 @@ if(isset( $_SESSION["login"]) && $_SESSION["login"] == "valid"){
     $result = $userLogin->login($_POST["user"],$_POST["password"]);
     if($result){
         $_SESSION["login"] = "valid";
-<<<<<<< HEAD
         $_SESSION['USER'] = $_POST["user"];
-=======
->>>>>>> origin/master
         header("Location: ../view/modules/instructor/instructor.php");
     }else{
         $_SESSION["login"] = "invalid";
